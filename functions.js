fc68534f50985bba892aa75438f981771a84d29b
ArrBee <ri.balfourth@gmail.com>
2022-08-29 16:17:31 +0100

    Added submit list-item function
    function takes the current value of the input and then creates a list item.
    List item is filled with the necessary buttons and then added to the list
    as a new first element (so that it is displayed first).
    List item buttons currently have no real functionality, but have been
    styled, details in commit to follow.

--- functions.js
+++ functions.js
@@ -1,8 +1,38 @@
 document.querySelector(".submit").addEventListener("click", () => {
-    const listItem = document.createElement("li");
-    const list = document.querySelector("list-section");
     const text = document.querySelector(".user-input").value;
-    listItem.innerHTML = `${text}`;
-    list.appendChild(listItem);
+    if (text == "") {
+        return;
+    }
+    const iconClasses = ["fa fa-pencil", "fa fa-trash",]
+    const buttonClasses = ["edit", "delete", "checkbox"]
+    const listItem = document.createElement("li");
+    const itemControls = document.createElement("div")
+    itemControls.setAttribute("class", "item-controls")
+    listItem.append(text);
 
+    for (let i = 0; i < 3; i++) {
+        const listButton = document.createElement("button");
+        if (i < 2) {
+            const icon = document.createElement("i");
+            icon.setAttribute("class", `${iconClasses[i]}`);
+            listButton.appendChild(icon);
+            listButton.setAttribute("class", "list-button");
+            listButton.setAttribute("id", `${buttonClasses[i]}`);
+            itemControls.appendChild(listButton)
+            console.log(listItem);
+        } else {
+            listButton.style.backgroundColor = "black";         
+            listButton.setAttribute("class", "list-button");
+            listButton.setAttribute("id", `${buttonClasses[i]}`);
+            itemControls.appendChild(listButton)
+        }
+    }
+
+
+    listItem.appendChild(itemControls);
+    listItem.setAttribute("class", "todo");
+    const list = document.querySelector(".todo-list");
+    list.insertBefore(listItem, list.firstElementChild);
+    document.querySelector(".user-input").value = "";
 })
+
